03bde96a6187670bde10547a9aec86a1f9664c61
Dev-Gony <gwonyi190000@gmail.com>
2026-09-24 19:44:12 +0900

    chore: version Solar Return analytics event

---
+++ supabase/migrations/20260924_extend_solar_return_events.sql
@@ -0,0 +1,23 @@
+alter table public.product_events
+  drop constraint if exists product_events_event_name_check;
+
+alter table public.product_events
+  add constraint product_events_event_name_check
+  check (
+    event_name in (
+      'landing_view',
+      'quick_started',
+      'quick_completed',
+      'detailed_opened',
+      'detailed_completed',
+      'save_clicked',
+      'result_saved',
+      'ask_prism_used',
+      'share_card_created',
+      'upgrade_detailed',
+      'feedback_submitted',
+      'destiny_timeline_opened',
+      'destiny_date_inspected',
+      'solar_return_calculated'
+    )
+  );
